3d2f57989aab9b3b70027d1b23c290eaa964c8ee
Dennismwendwa <dennismusembi2@gmail.com>
2023-08-28 13:02:32 +0300

    created function that creates binary tree nodes

--- binary_trees.h
+++ binary_trees.h
@@ -22,21 +22,12 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/* function to print binary tree in pretty way */
-
-typedef struct binary_tree_s
-{
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
-
 void binary_tree_print(const binary_tree_t *);
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
