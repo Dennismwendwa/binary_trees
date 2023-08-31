db37922058be4648304bf4d030a3ec59a0e48265
Ada <106437177+Coding-doves@users.noreply.github.com>
2023-08-31 21:31:44 +0100

    Update 130-binary_tree_is_heap.c

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,7 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_heap - function that checks if a binary tree is a valid
- * Max Binary Heap
+ * binary_tree_is_heap - function that checks if a binary tree is valid
  * @tree: is a pointer to the root node of the tree to check
  * Return: 0 if tree is NULL, or 1
  */
